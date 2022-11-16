65cc085aac4b19f99c0571d1356e1674104a93a2
KneiseS <sebastian.kneise@lv1871.de>
2022-11-16 14:38:24 +0100

    refactor problem page

--- apps/mars-rover/src/app/app.module.ts
+++ apps/mars-rover/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatExpansionModule} from '@angular/material/expansion';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -7,9 +8,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
 import {ExerciseComponent} from "./exercise/exercise.component";
 
+
 @NgModule({
   declarations: [AppComponent, ExerciseComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatButtonModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatButtonModule, MatExpansionModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- apps/mars-rover/src/app/mars-rover/rover.ts
+++ apps/mars-rover/src/app/mars-rover/rover.ts
@@ -1,6 +1,4 @@
 export type Direction = 'north' | 'east' | 'south' | 'west';
-export type Rotation = 'clockwise' | 'counter-clockwise';
-export type Movement = 'forward' | 'backward'
 
 export interface Rover {
   position: {
